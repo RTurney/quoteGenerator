bb76957070e34bea47d3fe30b41a517572fd4699
RTurney <r.turney93@gmail.com>
2021-02-18 10:08:26 +0000

    Random Quote Generator built. 1 test green.

--- spec/quoteGeneratorSpec.js
+++ spec/quoteGeneratorSpec.js
@@ -2,5 +2,8 @@ describe('Quote generator', () => {
 
   describe('quote generator', () =>{
 
+    it('will return a string', () => {
+      expect(quoteGenerator()).toBeInstanceOf(String);
+    });
   });
 });
